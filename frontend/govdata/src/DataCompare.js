b6217dc0fc43f20786e1d547e2e663901644fac3
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2021-07-23 22:33:35 -0300

    Add inline Data Summary when comparing data

--- frontend/govdata/src/DataCompare.js
+++ frontend/govdata/src/DataCompare.js
@@ -6,6 +6,7 @@ import ReactModal from 'react-modal';
 import ModalContent from './ModalContent';
 import DataBarChart from './DataBarChart';
 import DataBarChartComparison from './DataBarChartComparison.js';
+import './DataCompare.css';
 
 import queryString from 'query-string';
 
@@ -186,12 +187,12 @@ class DataCompare extends React.Component{
 					<BackButton/>
 
 					<button id="all-data-btn" className="btn" onClick={this.handleOpenDataModal}>All Data</button>
-
-					<DataSummary name={this.state.data1[0]["Nome Órgão Subordinado"]} key={this.entity_id1} data={this.state.data1} values_summary={this.state.values_summary1} data_keys={this.state.data_keys}/>
-					<br></br>
-					<br></br>
-					<DataSummary name={this.state.data2[0]["Nome Órgão Subordinado"]} key={this.entity_id2} data={this.state.data2} values_summary={this.state.values_summary2} data_keys={this.state.data_keys}/>
-
+					<div className="summary-container">
+						<DataSummary name={this.state.data1[0]["Nome Órgão Subordinado"]} key={this.entity_id1} data={this.state.data1} values_summary={this.state.values_summary1} data_keys={this.state.data_keys}/>
+						<br></br>
+						<br></br>
+						<DataSummary name={this.state.data2[0]["Nome Órgão Subordinado"]} key={this.entity_id2} data={this.state.data2} values_summary={this.state.values_summary2} data_keys={this.state.data_keys}/>
+					</div>
 					<DataBarChartComparison
 						data_keys={this.state.data_keys}
 						all_transactions_data_1={this.state.data1}
