bdb04b11e4bdd31f8392f8dd7c2e2285ad7d95c0
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2021-08-27 20:52:11 -0300

    Partial commit. Improve DataProcessor.

--- collector/data_processor/data_processor.py
+++ collector/data_processor/data_processor.py
@@ -3,6 +3,7 @@ import json
 
 from collector.db_connector import db_connector
 from collector.db_connector import redis_connector
+from collector.data_inspector import data_inspector
 
 
 """
@@ -26,21 +27,65 @@ class DataProcessor():
             rank_size: number os entities to put in the rank.
             date_filter: list of two dates to use as filter in DB query.
         """
-        pass
+        logging.debug("Creating Receivers Rank")
+        self.__connect_redis(db=1)
+        
+        receivers_redis_key = "Subordinado_list"
+        receivers_list = json.loads(self.redis_connector.get(receivers_redis_key))
+        for receiver in receivers_list:
+            r_data = self.__get_data_for_single_receiver(receiver['Código Órgão Subordinado'], "Subordinado") 
+            r_total_value = self.__sum_receiver_values(r_data, "Valor Pago (R$)")
 
-    def create_entities_list(self):
+        logging.debug("Done.")
+        return
+
+    def __sum_receiver_values(self, receiver_data, value_key):
+        """
+        Sum all values received for a single Receiver.
+        Args:
+            receiver_data: LIST od dicts, with receiver entrances at DB.
+            value_key: key dict field representing values received.
+        """
+        total_value = 0
+        for single_d in receiver_data:
+            #parsing value, cause it comes as 'XX,XXXXX' from DB
+            value_str = single_d[value_key][:-2]
+            value_str = value_str.replace(",", ".")
+            total_value += float(value_str)
+        return total_value
+
+
+    def __get_data_for_single_receiver(self, receiver_id, entity_type):
+        """
+        Get from MongoDB data of a single Receiver Entity.
+        Args:
+            receiver_id: (int OR str) Entity ID ('ID Órgão Suberdinado')
+        """
+        di = data_inspector.DataInspector(self.db_connector)
+        return di.get_entity_data(entity_id=receiver_id, entity_type=entity_type, date="")
+
+
+    def create_entities_list(self, entity_type=None):
         """
         Create a list with all Entities (name, ID), removind duplicated.
         Save this list inside a RedisDB (db=1) to be used as a cache system
         by the API.
         """
-        all_entities = self.__get_all_entities(entity_type='Subordinado',
+        if not entity_type:
+            logging.warning("No entity type selected at 'create_entities_list'. Returning...")
+            return
+
+        all_entities = self.__get_all_entities(entity_type=entity_type,
                                                date=None)
         self.__connect_redis(db=1)
         
-        #TODO unicoded JSOM Dump!
-        return self.redis_connector.set("entities_list", json.dumps(all_entities))
+        #TODO use date filtering as part of the key name
+        key_name = entity_type + "_list"
+        
+        #TODO fix unicode in JSOM Dump str
+        return self.redis_connector.set(key_name, json.dumps(all_entities))
 
+    #TODO WARNING: duplicated code. Already inside DataInspector class.
     def __get_all_entities(self, entity_type=None, date=None):
         """
         Get all entities names and IDs.
@@ -64,9 +109,11 @@ class DataProcessor():
         result = self.db_connector.query(filter=query_filter, fields=query_fields)
         return self.__transform_data_in_list(query_result=result, entity_type=entity_type, remove_duplicated=True)
 
+    #TODO WARNING: duplicated code. Already inside DataInspector class.
     def __parse_date(self, date):
         return date[:4] + "/" + date[4:]
 
+    #TODO WARNING: duplicated code. Already inside DataInspector class.
     def __transform_data_in_list(self, query_result, entity_type, remove_duplicated=False):
         """
         Return a LIST with all data collected.
@@ -89,6 +136,7 @@ class DataProcessor():
             all_data = self.__remove_duplicated(all_data, entity_type)
         return all_data
 
+    #TODO WARNING: duplicated code. Already inside DataInspector class.
     def __remove_duplicated(self, original_list, entity_type):
         """
         Remove duplicated entries from BD.
