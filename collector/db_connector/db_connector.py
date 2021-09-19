9974d134da18876f6560f2b201549053437d7770
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2021-09-19 19:31:22 -0300

    Adjust DB address for Docker

--- collector/db_connector/db_connector.py
+++ collector/db_connector/db_connector.py
@@ -7,7 +7,7 @@ class DbConnector():
         self.mucol = None
 
     def connect(self):
-        self.myclient = pymongo.MongoClient(connect=False, host='localhost', port=27017)
+        self.myclient = pymongo.MongoClient(connect=False, host='mongo', port=27017)
         self.mydb = self.myclient["govdata"]
         self.mycol = self.mydb["reports-data"]
 
