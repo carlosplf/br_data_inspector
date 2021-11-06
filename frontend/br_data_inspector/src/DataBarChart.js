64940a78a12884dc477c1afb80e997cc80a0f6e3
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2021-11-06 15:58:11 -0300

    Adjust CSS for mobile.

--- frontend/br_data_inspector/src/DataBarChart.js
+++ frontend/br_data_inspector/src/DataBarChart.js
@@ -6,7 +6,8 @@ import {
 	VerticalGridLines,
 	HorizontalGridLines,
 	VerticalBarSeries,
-    Hint
+    Hint,
+    FlexibleWidthXYPlot
 } from 'react-vis';
 import "./DataBarChart.css";
 import DataChartLabel from "./DataChartLabel";
@@ -131,7 +132,7 @@ class DataBarChart extends React.Component{
 			<div className="DataBarChart">
             <link rel="stylesheet" href="https://unpkg.com/react-vis/dist/style.css"/>
 				<DataChartLabel entities={entities} keys={this.keys_to_show} colors={this.graph_bar_colors}/>
-				<XYPlot xType="ordinal" style={{marginTop: 20}} width={1200} height={600} margin={{left: 120}}>
+				<FlexibleWidthXYPlot xType="ordinal" style={{marginTop: 20}} height={600} margin={{left: 120}}>
 				<VerticalGridLines />
 				<HorizontalGridLines />
 				<XAxis 
@@ -144,7 +145,7 @@ class DataBarChart extends React.Component{
 				}}/>
 				{all_data_series}
                 {hint}
-				</XYPlot>
+				</FlexibleWidthXYPlot>
 				<br></br>
 			</div>
 		)
