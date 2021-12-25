495933623b26362a24e0e970c14c310108704872
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2021-12-25 18:08:58 -0300

    Save collector log messages in a file.

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,27 @@
 import logging
 import argparse
+from logging.handlers import RotatingFileHandler
 from collector import collector
 from collector.data_processor import data_processor
 
-logging.basicConfig(level=logging.DEBUG)
+
+LOG_FILE="./logs/br_data_collector_backend_log"
+
+
+log_handler = RotatingFileHandler(
+    LOG_FILE,
+    mode='a',
+    maxBytes=5*1024*1024,
+    backupCount=1,
+    encoding=None,
+    delay=0
+)
+
+log_handler.setLevel(logging.DEBUG)
+app_log = logging.getLogger()
+app_log.setLevel(logging.DEBUG)
+app_log.addHandler(log_handler)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-cl", "--createlists",
@@ -42,6 +60,7 @@ if args.createlists:
 
 if args.collect:
     call_collector()
+
 
 if args.update:
     call_updater()
