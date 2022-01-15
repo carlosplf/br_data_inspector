5545fbc53ba2d13ffa43cc3b76039910dd7257c9
Carlos Pereira Lopes Filho <carlosplfilho@gmail.com>
2022-01-15 10:18:44 -0300

    Fix page title.

--- frontend/br_data_inspector/src/RankPage/RankPage.js
+++ frontend/br_data_inspector/src/RankPage/RankPage.js
@@ -9,7 +9,7 @@ class RankPage extends React.Component{
     render(){
         return(
             <div class="rankPage">
-                <Header header_text="Maiores Recebedores" dark_background={true}/>
+                <Header header_text="Maiores despesas" dark_background={true}/>
                 <div className="pageTitleRank">
                     <h2 style={{fontWeight: 'bold'}}>Soma de valores PAGOS por Órgão Subordinado.</h2>
                     <p>2021: Considerando dados até Junho.</p>
